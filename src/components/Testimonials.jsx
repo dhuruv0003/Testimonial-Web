52ebd0bb565b1df75f5731309eee489f02a63ca5
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-20 01:54:17 +0530

    "new commit"

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,19 +1,32 @@
 import Card from "./Card"
-import { FaChevronLeft,FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-export default function Testimonial(props){ 
-    let reviews=props.reviews
+export default function Testimonial(props) {
+    let reviews = props.reviews
+
+    function leftshifthandler(){
+
+    }
+    function rightshifthandler(){
+        
+    }
+    function surprisehandler(){
+        
+    }
+
     return (
         <div className="">
             <Card review={reviews[0]}></Card>
             <div className=" flex text-3xl justify-center mt-5 gap-3 text-violet-400 items-center font-bold">
-                <button className=" hover:text-violet-600"> <FaChevronLeft/> </button>
-                <button className="hover:text-violet-600">
-                <FaChevronRight/>
+                <button 
+               onClick={leftshifthandler} className=" hover:text-violet-600"> <FaChevronLeft /> </button>
+                <button onClick={rightshifthandler}  className="hover:text-violet-600">
+                    <FaChevronRight />
                 </button>
             </div>
             <div className="">
-                <button className="bg-violet-400 hover:bg-violet-600 transition-all duration-200 text-white font-bold px-10 py-2 rounded-md mt-5">Surprise Me</button>
+                <button 
+                onClick={surprisehandler}className="bg-violet-400 hover:bg-violet-600 transition-all duration-200 text-white font-bold px-10 py-2 rounded-md mt-5">Surprise Me</button>
             </div>
         </div>
     )
