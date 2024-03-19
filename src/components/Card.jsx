8b8c17466fb772ff4e24c5f972baefb812f2036f
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-20 01:50:25 +0530

    "new commit"

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
-import { FaChevronLeft,FaChevronRight } from "react-icons/fa";
+
 
 export default function Card(props){
     let review=props.review
@@ -24,15 +24,7 @@ export default function Card(props){
             <div className="text-violet-400  mx-auto mt-5">
                 <FaQuoteRight></FaQuoteRight>
             </div>
-            <div className=" flex text-3xl justify-center mt-5 gap-3 text-violet-400 items-center font-bold">
-                <button className=" hover:text-violet-600"> <FaChevronLeft/> </button>
-                <button className="hover:text-violet-600">
-                <FaChevronRight/>
-                </button>
-            </div>
-            <div className="">
-                <button className="bg-violet-400 hover:bg-violet-600 transition-all duration-200 text-white font-bold px-10 py-2 rounded-md mt-5">Surprise Me</button>
-            </div>
+            
         </div>
 
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,20 @@
 import Card from "./Card"
+import { FaChevronLeft,FaChevronRight } from "react-icons/fa";
 
 export default function Testimonial(props){ 
     let reviews=props.reviews
     return (
         <div className="">
             <Card review={reviews[0]}></Card>
+            <div className=" flex text-3xl justify-center mt-5 gap-3 text-violet-400 items-center font-bold">
+                <button className=" hover:text-violet-600"> <FaChevronLeft/> </button>
+                <button className="hover:text-violet-600">
+                <FaChevronRight/>
+                </button>
+            </div>
+            <div className="">
+                <button className="bg-violet-400 hover:bg-violet-600 transition-all duration-200 text-white font-bold px-10 py-2 rounded-md mt-5">Surprise Me</button>
+            </div>
         </div>
     )
 }
