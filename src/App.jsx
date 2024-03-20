e8b1d45196a2e48217b74c33a8e9ab02ffb0d315
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-20 20:52:31 +0530

    "Last Commit"

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className='flex flex-col  w-[100vw] h-[100vh] justify-center items-center bg-[#14213d]'>
       <div className="text-center">
-        <h1 className='text-4xl text-[#fffcf2] font-bold'>Our Testimonial</h1>
+        <h1 className='text-4xl text-[#fffcf2] font-bold  '>Our Testimonial</h1>
         <div className=" bg-[#ffa20e] h-[4px]  
-        w-1/5 mx-auto mt-3"></div>
+        w-1/5 mx-auto mt-3 test"></div>
         <Testimonial reviews={reviews}></Testimonial>
       </div>
     </div>
